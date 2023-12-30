61885e9f151e72c63aa7675a9cd32ae48c20ac7b
rohan9024 <rohanwandre24@gmail.com>
2023-12-30 11:35:55 +0530

    Added donations half

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -27,19 +27,19 @@ function page() {
 
     return (
         <>
-        <Navbar />
+            <Navbar />
             <motion.div
                 initial={{ opacity: 0, y: -120 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ ease: "easeOut", duration: 1 }}
                 className='lg:flex-row flex-col flex justify-center lg:justify-center items-center  lg:space-x-32   mx-10 lg:mt-20'>
-       <Image
+                <Image
                     src="/contactBackground.jpg"
                     width={3000}
                     height={3000}
                     alt="sideimage"
-                    className=' object-contain h-auto w-1/2 hidden lg:flex' 
-                    />
+                    className=' object-contain h-auto w-1/2 hidden lg:flex'
+                />
 
 
                 <div className=' flex lg:hidden w-screen'>
@@ -51,19 +51,19 @@ function page() {
                         className=' object-cover h-[350px]'
                     />
                 </div>
-                    <div className='flex flex-col justify-center items-center mt-20 space-y-5 lg:w-1/2'>
-                        <h1 className={`${raleway.className} text-4xl font-bold mb-10`}>Contact Us</h1>
-                        <form className='flex flex-col justify-center items-center'>
-                            <input onChange={(e) => setEmail(e.target.value)} required type="text" placeholder="Enter Email" className={`${manrope.className} placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96`} />
-                            <input onChange={(e) => setDesc(e.target.value)} required type="text" placeholder="Description" className={`${manrope.className}  mt-10 placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96 `} />
+                <div className='flex flex-col justify-center items-center mt-20 space-y-5 lg:w-1/2'>
+                    <h1 className={`${raleway.className} text-4xl font-bold mb-10`}>Contact Us</h1>
+                    <form className='flex flex-col justify-center items-center'>
+                        <input onChange={(e) => setEmail(e.target.value)} required type="text" placeholder="Enter Email" className={`${manrope.className} placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96`} />
+                        <input onChange={(e) => setDesc(e.target.value)} required type="text" placeholder="Description" className={`${manrope.className}  mt-10 placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96 `} />
 
 
-                            <div disabled={!email || !desc} type="submit" onClick={submit} class="cursor-pointer mt-10 px-36 py-4 bg-black text-white rounded-lg">
-                                <span class="relative text-center">Submit</span>
-                            </div>
+                        <div disabled={!email || !desc} type="submit" onClick={submit} class="cursor-pointer mt-10 px-36 py-4 bg-black text-white rounded-lg">
+                            <span class="relative text-center">Submit</span>
+                        </div>
 
-                        </form>
-                    </div>
+                    </form>
+                </div>
 
             </motion.div>
 
--- src/app/donate-now/page.js
+++ src/app/donate-now/page.js
@@ -9,66 +9,117 @@ import Navbar from '../../../components/Navbar';
 
 
 const raleway = Raleway({
-    weight: ['400', '700'],
-    subsets: ['latin'],
+  weight: ['400', '700'],
+  subsets: ['latin'],
 });
 
 const manrope = Manrope({
-    weight: ['400', '500', '600', '700', '800'],
-    subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+  subsets: ['latin'],
 });
 function page() {
-    const [desc, setDesc] = useState("")
-    const [email, setEmail] = useState("")
-    const submit = () => {
+  const [desc, setDesc] = useState("")
+  const [email, setEmail] = useState("")
+  const [paymentOption, setPaymentOption] = useState("recurring")
+  const [paymentDuration, setPaymentDuration] = useState("")
+  const paymentDurationList = [
+    "Monthly",
+    "Yearly"
+  ]
 
-    }
+  const handlePaymentDurationDropdown = (event) => {
+    setPaymentDuration(event.target.value);
+  };
+
+  const submit = () => {
+
+  }
 
 
-    return (
-        <>
-        <Navbar />
+  return (
+    <>
+      <Navbar />
+      <motion.div
+        initial={{ opacity: 0, y: -120 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ ease: "easeOut", duration: 1 }}
+        className=' flex-col flex justify-center lg:justify-center items-center  lg:space-x-32   mx-10 lg:mt-20'>
+
+        <div className='flex flex-col justify-center items-center mt-20 space-y-5 lg:w-1/2'>
+          <h1 className={`${raleway.className} text-4xl font-bold mb-10`}>Donate Now</h1>
+          <motion.div className='flex justify-center items-center cursor-pointer border border-gray-300 rounded-2xl'>
             <motion.div
-                initial={{ opacity: 0, y: -120 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ ease: "easeOut", duration: 1 }}
-                className='lg:flex-row flex-col flex justify-center lg:justify-center items-center  lg:space-x-32   mx-10 lg:mt-20'>
-                <Image
-                    src="/donateBackground.jpg"
-                    width={3000}
-                    height={3000}
-                    alt="sideimage"
-                    className=' object-contain h-auto w-1/2 hidden lg:flex' 
-                    />
-
-
-                <div className=' flex lg:hidden w-screen'>
-                    <Image
-                        src="/donateBackground.jpg"
-                        width={3000}
-                        height={3000}
-                        alt="sideimage"
-                        className=' object-cover h-[350px]'
-                    />
-                </div>
-                    <div className='flex flex-col justify-center items-center mt-20 space-y-5 lg:w-1/2'>
-                        <h1 className={`${raleway.className} text-4xl font-bold mb-10`}>Donate Now</h1>
-                        <form className='flex flex-col justify-center items-center'>
-                            <input onChange={(e) => setEmail(e.target.value)} required type="text" placeholder="Enter Email" className={`${manrope.className} placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96`} />
-                            <input onChange={(e) => setDesc(e.target.value)} required type="text" placeholder="Description" className={`${manrope.className}  mt-10 placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96 `} />
-
-
-                            <div disabled={!email || !desc} type="submit" onClick={submit} class="cursor-pointer mt-10 px-36 py-4 bg-black text-white rounded-lg">
-                                <span class="relative text-center">Submit</span>
-                            </div>
-
-                        </form>
-                    </div>
-
+              className={`${paymentOption === 'one time'
+                ? 'bg-blue-600 border-blue-600 text-white'
+                : 'bg-none'
+                } flex justify-center items-center  px-32 py-2 rounded-2xl`}
+              onClick={() => setPaymentOption('one time')}
+              whileHover={{ scale: 1.1 }} // Add animation on hover
+              transition={{ type: 'linear', stiffness: 300 }}
+            >
+              <h1 className={`${manrope.className} text-xl`}>One Time</h1>
             </motion.div>
+            <motion.div
+              className={`${paymentOption === 'recurring'
+                ? 'bg-blue-600 border-blue-600 text-white'
+                : 'bg-none'
+                } flex justify-center items-center px-32 py-2 rounded-2xl`}
+              onClick={() => setPaymentOption('recurring')}
+              whileHover={{ scale: 1.1 }} // Add animation on hover
+              transition={{ type: 'linear', stiffness: 300 }}
+            >
+              <h1 className={`${manrope.className} text-xl`}>Recurring</h1>
+            </motion.div>
+          </motion.div>
 
-        </>
-    )
+          {/* Recurring */}
+          <div className='flex justify-center items-center space-x-16'>
+
+          <div className='flex flex-col justify-start space-y-5'>
+            <h1 className={`${manrope.className} text-xl`}>How often would you like to donate?</h1>
+            <select
+              value={paymentDuration}
+              onChange={handlePaymentDurationDropdown}
+              className="block w-96 py-2 px-5 leading-tight border border-gray-700 focus:outline-none cursor-pointer"
+            >
+              {paymentDurationList.map((paymentDuration, index) => (
+                <option key={index} value={paymentDuration}>
+                  {paymentDuration}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className='flex flex-col justify-start space-y-5'>
+            <h1 className={`${manrope.className} text-xl`}>For how long would you like to donate?</h1>
+            <select
+              value={paymentDuration}
+              onChange={handlePaymentDurationDropdown}
+              className="block w-96 py-2 px-5 leading-tight border border-gray-700 focus:outline-none cursor-pointer"
+            >
+              {paymentDurationList.map((paymentDuration, index) => (
+                <option key={index} value={paymentDuration}>
+                  {paymentDuration}
+                </option>
+              ))}
+            </select>
+          </div>
+          </div>
+
+
+          <input onChange={(e) => setEmail(e.target.value)} required type="text" placeholder="Enter Email" className={`${manrope.className} placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96`} />
+          <input onChange={(e) => setDesc(e.target.value)} required type="text" placeholder="Description" className={`${manrope.className}  mt-10 placeholder:text-gray-800 px-5 py-2  outline-none border border-gray-800 w-96 `} />
+
+
+          <div disabled={!email || !desc} type="submit" onClick={submit} class="cursor-pointer mt-10 px-36 py-4 bg-black text-white rounded-lg">
+            <span class="relative text-center">Submit</span>
+          </div>
+
+        </div>
+
+      </motion.div>
+
+    </>
+  )
 }
 
 export default page

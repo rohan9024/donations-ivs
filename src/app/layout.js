63e35ab07783aae7f97a769caf5cf6c7eed14bcb
rohan9024 <rohanwandre24@gmail.com>
2024-02-03 09:51:01 +0530

    Fixing deployment issues #2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
+"use client"
 
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'IVS Donations',
-}
+// export const metadata = {
+//   title: 'IVS Donations',
+// }
 
 export default function RootLayout({ children }) {
 
